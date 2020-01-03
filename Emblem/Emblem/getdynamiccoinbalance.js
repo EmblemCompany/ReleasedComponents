27d398f95f34844720ef0197ff328ba865788aa5
Dawn Test Code <dawn.test.code@gmail.com>
2020-01-03 16:12:56 -0500

    Updated the icon for emblem component

--- Emblem/Emblem/getdynamiccoinbalance.js
+++ Emblem/Emblem/getdynamiccoinbalance.js
@@ -6,7 +6,7 @@ exports.input =true;
 exports.output =1;
 exports.version ="0.0.6";
 exports.author ="Shannon Code";
-exports.icon ="btc";
+exports.icon ="coins";
 exports.options ={
     service: "dexray2",
     host: "api.emblemvault.io"
