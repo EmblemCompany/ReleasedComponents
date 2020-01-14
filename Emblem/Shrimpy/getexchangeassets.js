202466ac2931483dac7383533cda1fd90fb55504
Shannon Code <genecyber@gmail.com>
2020-01-14 12:37:51 -0500

    main sendmail now generic smtp, added shrimpy documentation

--- Emblem/Shrimpy/getexchangeassets.js
+++ Emblem/Shrimpy/getexchangeassets.js
@@ -9,6 +9,7 @@ exports.author = 'Shannon Code <shannon@unspecified.me>';
 exports.icon = 'coins';
 exports.version = '1.0.0';
 exports.options = {  };
+exports.npm = ["shrimpy-node"];
 exports.html = `<div class="padding">
     <div class="row">
         <div class="col-md-6">
@@ -19,13 +20,18 @@ exports.html = `<div class="padding">
 
 exports.readme = `# Shrimpy Get Exchange Assets
 
+This endpoint retrieves exchange asset information for the selected exchange.
+
+## Fields
+
+*Select an Exchange* (required): This is the exchange you want to get asset popularity from.
+
 [Shrimpy Documentation](https://developers.shrimpy.io/docs/#get-exchange-assets)
 `;
 
 exports.install = function(instance) {
     const Shrimpy = require('shrimpy-node');
     const publicClient = new Shrimpy.ShrimpyApiClient();
-    // const privateClient = new Shrimpy.ShrimpyApiClient(publicKey, privateKey);
 	instance.on('data', (response) => {
         publicClient.getExchangeAssets(instance.options.exchange).then(assets =>{
             response.data = JSON.parse(JSON.stringify(assets));
--- Emblem/Shrimpy/getorderbooks.js
+++ Emblem/Shrimpy/getorderbooks.js
@@ -9,6 +9,7 @@ exports.author = 'Shannon Code <shannon@unspecified.me>';
 exports.icon = 'chart-pie';
 exports.version = '1.0.0';
 exports.options = {  };
+exports.npm = ["shrimpy-node"];
 exports.html = `<div class="padding">
     <div class="row">
         <div class="col-md-6">
@@ -17,10 +18,10 @@ exports.html = `<div class="padding">
     </div>
     <div class="row">
         <div class="col-md-3">
-            <div data-jc="textbox" data-jc-path="baseSymbol" data-jc-config="placeholder:xlm;required:true">@(Base Symbol) </div>
+            <div data-jc="textbox" data-jc-path="baseSymbol" data-jc-config="placeholder:xlm;required:false">@(Base Symbol) </div>
         </div>
         <div class="col-md-3">
-            <div data-jc="textbox" data-jc-path="quoteSymbol" data-jc-config="placeholder:btc;required:true">@(Quote Symbol) </div>
+            <div data-jc="textbox" data-jc-path="quoteSymbol" data-jc-config="placeholder:btc;required:false">@(Quote Symbol) </div>
         </div>
         <div class="col-md-3">
             <div data-jc="textbox" data-jc-path="limit" data-jc-config="type:number;increment:true;placeholder:10">@(Number of Order Books) </div>
@@ -29,6 +30,19 @@ exports.html = `<div class="padding">
 </div>`;
 
 exports.readme = `# Shrimpy Get Order Books
+
+This component retrieves live order book data for the exchange specified.
+
+## Fields
+
+*Select an Exchange* (required): This is the exchange you want to get asset popularity from.
+
+*Base Symbol* (optional): The base symbol. (e.g. XLM for a XLM-BTC market) quantity is in these units.
+
+*Quote Symbol* (optional): The quote symbol. (e.g. BTC for a XLM-BTC market).
+
+*Limit* (optional): The maximum number of asks and bids to retrieve. Defaults to 10 if not supplied.
+
 
 [Shrimpy Documentation](https://developers.shrimpy.io/docs/#get-order-books)
 `;
@@ -40,8 +54,8 @@ exports.install = function(instance) {
 	instance.on('data', (response) => {
         publicClient.getOrderBooks(
             instance.options.exchange,  // exchange
-            instance.options.baseSymbol,      // baseSymbol
-            instance.options.quoteSymbol,      // quoteSymbol
+            instance.options.baseSymbol || '',      // baseSymbol
+            instance.options.quoteSymbol || '',      // quoteSymbol
             instance.options.limit || 10          // limit
         ).then(orderBooks =>{
             response.data = JSON.parse(JSON.stringify(orderBooks, null, 4));
