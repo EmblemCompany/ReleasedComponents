6a1411e90ae935cbefe0dcbea6c22b3e4b87db37
Shannon Code <genecyber@gmail.com>
2020-01-23 16:22:46 -0500

    simplified and proxied clicksend

--- Templates/Components/sms_sendsms.js
+++ Templates/Components/sms_sendsms.js
@@ -4,7 +4,7 @@ exports.group ="Template Components";
 exports.color ="#49cc90";
 exports.input =true;
 exports.output =1;
-exports.version ="0.0.1";
+exports.version ="0.0.2";
 exports.author ="Shannon Code";
 exports.icon ="sms";
 
@@ -29,117 +29,32 @@ exports.html = `<div class="padding">
 </div>
 `;
 
-var request = {
-    "auth-removed": {
-        "type": "basic",
-        "basic": [
-            {
-                "key": "password",
-                "value": "{{password}}",
-                "type": "string"
-            },
-            {
-                "key": "username",
-                "value": "{{username}}",
-                "type": "string"
-            }
-        ]
-    },
-    "method": "POST",
-    "header": [
-        {
-            "key": "Content-Type",
-            "value": "application/json"
-        }, {
-            "key": "Authorization",
-            "value": "Basic aGVsbG9AdW5zcGVjaWZpZWQubWU6VU5zcDNjaWZpM2QxNyE="
-        }
-    ],
-    "body": [
-        {
-            "key": "messages",
-            "value": [
-                {
-                    "source": "php",
-                    "body": "Jelly liquorice marshmallow candy carrot cake 4Eyffjs1vL.",
-                    "to": "+17574691070"
-                },
-                {
-                    "source": "php",
-                    "body": "Chocolate bar icing icing oat cake carrot cake jelly cotton MWEvciEPIr.",
-                    "list_id": 428
-                }
-            ]
-        }
-    ],
-    "url": {
-        "raw": "https://rest.clicksend.com/v3/sms/send",
-        "protocol": "https",
-        "host": [
-            "rest",
-            "clicksend",
-            "com"
-        ],
-        "path": [
-            "v3",
-            "sms",
-            "send"
-        ]
-    }
-};
 exports.install =function(instance) {
 
-	instance.on('data', function(response) {
-        theRequest = request
+	instance.on('data', function(flowdata) {
+        
         RESTBuilder.make(function(builder) {
-			var url = generateUrl(theRequest)
+            var url = 'https://api.emblemvault.io/clicksend'
             builder.url(url);
-            if (instance.options.service || response.data.service || FLOW.variables.service ) {
-                builder.header('service', instance.options.service || response.data.service || FLOW.variables.service)
-            }
-            if (theRequest.header) {
-                theRequest.header.forEach(header=>{
-                    builder.header(header.key, header.value)
-                })                
-            }
-            if (theRequest.auth) {
-                if (instance.options['username']) {
-                    if (instance.options['username'].includes('msg.')) {
-                        instance.options['username'] = response.data[instance.options["username"].replace('msg.', '')]
-                    }
-                } 
-                if (instance.options['password']) {
-                    if (instance.options['password'].includes('msg.')) {
-                        instance.options['password'] = response.data[instance.options["password"].replace('msg.', '')]
-                    }
-                }
-                builder.auth(instance.options['username'] || response.data.username || FLOW.variables.username, instance.options['password'] || response.data.password || FLOW.variables.password)
-            }
-            var body = {}
-            if (theRequest.body) {
-                var body = {}
-                body.messages = []
-                var smsBody = instance.options.msg || response.data.msg || FLOW.variables.msg
-                if (smsBody.includes('{msg.')) {
-                    smsBody = replaceTokenizedString(response, smsBody)
-                }
-                body.messages.push(
-                    {
-                        to: instance.options.to || response.data.to || FLOW.variables.to,
-                        body: smsBody
-                    }
-                )
-                builder.json(body);      
-            }
-            builder.method(theRequest.method.toLowerCase() || 'get')
+            builder.header('service', 'cbproxy')
+            builder.method('post')
+            builder.json({
+                "messages":[ 
+                    { 
+                        "source":"Circuit Builder", 
+                        "body":replaceTokenizedString(flowdata, instance.options.msg || flowdata.data.msg), 
+                        "to":replaceTokenizedString(flowdata, instance.options.to || flowdata.data.to)
+                    } 
+                ]
+            });
             builder.exec(function(err, api_response) {
-                response.data = api_response;
-                if (instance.options.downstream) {
-                    response.set(instance.name, response.data);
+                flowdata.data = api_response
+				if (instance.options.downstream) {
+                    flowdata.set(instance.name, flowdata.data);
                 }
-                instance.send(response);
-			});
-		});
+				instance.send(flowdata)
+            });
+        });
 	});
 
 	instance.custom.send = function(message) {
@@ -156,52 +71,12 @@ exports.install =function(instance) {
     function replaceTokenizedString(response, myString) {
         var tokenRegex = /[^{\}]+(?=})/g
         var replaceArray = myString.match(tokenRegex);
-
-        replaceArray.forEach(item=>{
-                objectPath = item.replace('msg.', 'response.data.')
-        		myString = myString.replace('{' + item + '}', eval(objectPath))
-        })
-        return myString
-    }
-    
-    function generateUrl(request) {
-        var url;
-        if (request.url.protocol) {
-            url = request.url.protocol + '://'
-        } else {
-            url = 'http://'
-        }
-        if (request.url.host) {
-            var host = request.url.host.join('.')
-            if (host.includes('{{host}}')) {
-                host = host.replace('{{host}}', instance.options.host || response.data.host || FLOW.variables.host)
-            }
-            url = url + host
-        }        
-        if (request.url.path) {
-            url = url +  '/' + request.url.path.join('/')
-        }
-        if (request.url.query) {
-            url = url + '?'
-            request.url.query.forEach(kvp => {
-                if (instance.options[kvp.key] || response.data[kvp.key]) {
-                    if (instance.options[kvp.key].includes('msg.')) {
-                        instance.options[kvp.key] = response.data[instance.options[kvp.key].replace('msg.', '')]
-                    }
-                    url = url + kvp.key + '=' + instance.options[kvp.key] || response.data[kvp.key] || FLOW.variables[kvp.key] + '&'
-                }
+        if (replaceArray) {
+            replaceArray.forEach(item=>{
+                    objectPath = item.replace('msg.', 'response.data.');
+                    myString = myString.replace('{' + item + '}', eval(objectPath));
             })
         }
-        if (request.url.variable) {
-            request.url.variable.forEach(kvp => {
-                if (instance.options[kvp.key] || response.data[kvp.key] || FLOW.variables[kvp.key]) {
-                    if (instance.options[kvp.key].includes('msg.')) {
-                        instance.options[kvp.key] = response.data[instance.options[kvp.key].replace('msg.', '')]
-                    }
-                    url = url.replace(':'+kvp.key, instance.options[kvp.key] || response.data[kvp.key] || FLOW.variables[kvp.key])
-                }
-            })
-        }
-        return url
+        return myString;
     }
 };
