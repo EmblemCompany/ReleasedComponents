331a12fdefcbfa128a91cbcaf5861ec6e6733e00
Shannon Code <genecyber@gmail.com>
2020-02-04 11:29:31 -0500

    transform object now supports merging upstream

--- Emblem/IO/transformobject.js
+++ Emblem/IO/transformobject.js
@@ -4,7 +4,7 @@ exports.group = "Data Manipulation";
 exports.color ="#764f78";
 exports.input =true;
 exports.output =1;
-exports.version ="0.0.1";
+exports.version ="0.0.2";
 exports.author ="Shannon Code";
 exports.icon ="map-signs";
 
@@ -12,6 +12,11 @@ exports.readme = '60000315222';
 
 exports.html = `
 <div class="padding">
+	<div class="row">
+		<div class="col-md-6 m">
+			<div data-jc="checkbox__merge">@(Merge Upstream)</div>
+		</div>
+	</div>
 	<div class="row">
 		<div class="col-md-6 m">
 			<div data-jc="textboxlist" data-jc-path="properties" data-placeholder="@(Type properties to include here)">@(Property)</div>
@@ -27,6 +32,11 @@ exports.install = function(instance) {
 	instance.on('data', function(response) {
 		if(instance.options.properties) {
 			var newObject = {};
+			if (instance.options.merge) {
+				var tmp = {data: response.data}
+				tmp.upstream = response.repository
+				response.data = tmp
+			}
 			instance.options.properties.forEach((prop, index)=>{
 				var toEval = 'response.data.' + prop.replace('msg.', '')
 				newObject[instance.options.name[index]] = eval(toEval)
