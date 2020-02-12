980637b1558a3d30c5e0c7fbaa85447fcd73a365
Dawn Test Code <dawn.test.code@gmail.com>
2020-02-12 11:11:00 -0500

    Updating Send Email

--- Emblem/Messaging/sendemail.js
+++ Emblem/Messaging/sendemail.js
@@ -4,7 +4,7 @@ exports.group = "Messaging";
 exports.color ="#00CED1";
 exports.input =true;
 exports.output =1;
-exports.version ="0.0.3";
+exports.version ="0.0.4";
 exports.author ="Shannon Code";
 exports.icon ="paper-plane";
 
@@ -39,7 +39,7 @@ exports.html = `<div class="padding">
         <h3>SMTP server information</h3>
         <div class="row">
             <div class="col-md-6">
-                <div data-jc="textbox" data-jc-path="host" data-jc-config="placeholder:Host (in the form of smtp.example.com">@(host) </div>
+                <div data-jc="textbox" data-jc-path="host" data-jc-config="placeholder:Host (in the form of smtp.example.com)">@(host) </div>
                 <div class="help">To inject data into this field, use {msg.<path>} syntax <a href="https://unspecifiedsupport.freshdesk.com/support/solutions/articles/60000182172-using-a-component-s-response-data" target="_blank">More info here</a></div>
             </div>
             <div class="col-md-6">
