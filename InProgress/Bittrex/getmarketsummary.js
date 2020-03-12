b7d93389ce083a7d7f9a2a4b057d41be7e5c4770
stacyhowerton <stacy@unspecified.me>
2020-03-12 17:28:28 -0400

    getmarketsummary now selectable component.  One left

--- InProgress/Bittrex/getmarketsummary.js
+++ InProgress/Bittrex/getmarketsummary.js
@@ -1,3 +1,9 @@
+const MARKETSTRIGGER = 'getBittrexMarkets';
+const { BittrexClient } = require('bittrex-node')
+let client = new BittrexClient({
+    apiKey: '',
+    apiSecret: ''
+});
 exports.id = 'getmarket';
 exports.title = 'Get Specific Martket 24Hr Summary';
 exports.group = 'Bittrex';
@@ -15,26 +21,34 @@ exports.html = `
 <div class="padding">
     <div class="row">
         <div class="col-md-6">
+            <div data-jc="dropdown" data-jc-path="markets" data-jc-config="datasource:marketlist;required:true;empty:" class="m">@(Market Name)</div>
         </div>
     </div>
-</div>`;
+</div>
+<script>
+    ON('open.getmarket', function(component, options) {
+        TRIGGER('getBittrexMarkets', 'marketlist');
+    });
+</script>`;
 
 exports.readme = '60000611323';
 
 exports.install = function(instance) {
-    const { BittrexClient } = require('bittrex-node')
-    let client = new BittrexClient({
-        apiKey: '',
-        apiSecret: ''
-    });
     instance.on('data', function(flowdata) {
         runIt(flowdata);
     });
 
     async function runIt(flowdata) {
-        let data = await client.marketSummary('BTC-ETH'); //TODO - make selectable
+        let data = await client.marketSummary(instance.options.markets);
 
         flowdata.data = data;
 		instance.send(flowdata);
     };
 };
+
+FLOW.trigger(MARKETSTRIGGER, function(next) {
+    client.markets().then(markets=> {
+        var marketinfo = markets.map(item=>{return {name: item.MarketName, id: item.MarketName}});
+        next(marketinfo);
+    });
+});
